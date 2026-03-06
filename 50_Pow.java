21e465fa714e1b58ea7f0ab2d8a38f59bffc4f49
Ajay <143615564+LeoAj2005@users.noreply.github.com>
2026-03-06 23:48:02 +0530

    Implement power function to calculate x^n

---
+++ 50_Pow.java
@@ -0,0 +1,22 @@
+class Solution {
+    public double myPow(double x, int n) {
+
+        double result = 1;
+        if(n<0)
+        {
+            x = 1/x;
+            n = -n;
+
+        }
+        while (n >0)
+        {
+            if (n%2 ==1)
+            {
+                result = result * x;
+            }
+            x = x * x;
+            n = n/2;
+        }
+        return result;
+    }
+}
