fbb56c8d4702db937f5976e46319063f6a68a838
Ajay <143615564+LeoAj2005@users.noreply.github.com>
2026-03-06 23:58:18 +0530

    Refactor myPow to Program in Main class

--- 50_Pow.java
+++ 50_Pow.java
@@ -1,22 +1,26 @@
-class Solution {
-    public double myPow(double x, int n) {
-
-        double result = 1;
-        if(n<0)
+class Main {
+    public static int Program (int x, int n) {
+        int result = 1;
+        if (n<0)
         {
-            x = 1/x;
+            x = x/2;
             n = -n;
-
         }
         while (n >0)
         {
-            if (n%2 ==1)
+            if (n%2==1)
             {
                 result = result * x;
             }
-            x = x * x;
+            x = x*x;
             n = n/2;
         }
         return result;
     }
+    public static void main(String[] args)
+    {
+        int x;
+        int n;
+        int ans = Program(2,2);
+        System.out.println(ans);    }
 }
